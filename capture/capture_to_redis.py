e8adb696490ac474f4c30a267b42cda42ba681e3
Feinte75 <glenn.feunteun@telecom-bretagne.eu>
2016-02-25 14:02:38 -0500

    Refactoring: improve code, directory structure and comments

--- capture/capture_to_redis.py
+++ capture/capture_to_redis.py
@@ -1,7 +1,5 @@
 import os
-import socket
 import io
-import struct
 import time
 import picamera
 import redis
@@ -19,22 +17,29 @@ with picamera.PiCamera() as camera:
 
         # Capture continuously in BytesIO in video mode
         stream = io.BytesIO()
+
+        # Connect to Redis
         r = redis.StrictRedis(host='127.0.0.1', port='6379')
         r.set('new_image', 'false')
 
         for foo in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
-            # Write the length of the capture to the stream and flush to
-            # ensure it actually gets sent
 
+            # Store image in redis if previously stored image has been recovered
             if r.get('new_image') == b'false':
+
+                # Get stream size corresponding to the image size
                 image_size = stream.tell()
                 r.set('image_size', image_size)
-                # Rewind the stream and send the image data over the wire
+
+                # Rewind the stream and store the image data base64 encoded to redis
                 stream.seek(0)
                 print('Sending image with size: %d' %image_size + ' to nginx')
+
                 image_data = stream.read()
                 r.set('image_data', base64.b64encode(image_data))
                 print('Image sent to redis! Take another')
+
+                # Store in Redis that a new image is available
                 r.set('new_image', 'true')
 
             # Reset the stream for the next capture
--- lua/websocket.lua
+++ lua/websocket.lua
@@ -1,30 +1,29 @@
 local server = require "resty.websocket.server"
-local struct = require "struct"
 local redis  = require "resty.redis"
 
 -- Create web socket
 local wb, err = server:new
 {
-  timeout = 5000,  -- in milliseconds
+  timeout = 1000,  -- in milliseconds
   max_payload_len = 1000000,
 }
 
 if not wb then
-  ngx.log(ngx.ERR, "failed to new websocket: ", err)
+  ngx.log(ngx.ERR, "Failed to new websocket: ", err)
   return ngx.exit(444)
 end
 
--- Receive frame from client
+-- Receive connecting frame from client
 local data, typ, err = wb:recv_frame()
 
 if not data then
-  ngx.log(ngx.ERR, "failed to receive a frame: ", err)
+  ngx.log(ngx.ERR, "Failed to receive a frame: ", err)
   return ngx.exit(444)
 end
 
-ngx.log(ngx.INFO, "received a frame of type ", typ, " and payload ", data)
+ngx.log(ngx.INFO, "Received a frame of type ", typ, " and payload ", data)
 
--- Create redis client
+-- Create redis client and connect to redis server
 local r = redis:new()
 r:set_timeout(1000)
 
@@ -32,18 +31,23 @@ local ok, err = r:connect("127.0.0.1", 6379)
 
 if not ok then
   ngx.log(ngx.ERR, "Failed to connect to redis: ", err)
-  return
+  return ngx.exit(444)
 end
 
 ngx.log(ngx.INFO, "Connected to Redis !")
 
+-- Loop : Get image from redis and send it to client
+-- loop parameters :
 local done = false
 local timeout = 0
-local max_increment = 0.002
--- Loop : Get image from redis and send it to client
+local wait_time = 0.002
 
-ok, err = r:set("new_image", "false")
-ngx.log(ngx.INFO, "Tryed to set new_image : ok : ", ok, "  err : ", err)
+--ok, err = r:set("new_image", "false")
+--if not ok then
+--  ngx.log(ngx.ERR, "Tried to set new_image boolean and got err : ", err)
+--  return ngx.exit(444)
+--end
+
 repeat
 
   -- TODO Implement some kind of ping pong messages
@@ -58,56 +62,72 @@ repeat
   timeout = 0
   -- Check if image has changed since last get
   repeat
-    ngx.log(ngx.INFO, "Waiting for image since: ", timeout)
-    ngx.sleep(max_increment)
-    timeout = timeout + max_increment
+    ngx.log(ngx.INFO, "Waiting for new image since: ", timeout)
+
+    ngx.sleep(wait_time)
+    timeout = timeout + wait_time
+
+    -- Check if a new image is available in Redis
     local new_image, err = r:get("new_image") 
+
     if not new_image then 
-      ngx.log(ngx.INFO, "Failed to recover new_image")
-      return
-    end
-    if new_image == ngx.null then
-      ngx.log(ngx.INFO, "new_image not found")
+      ngx.log(ngx.ERR, "Failed to recover new_image")
+      break
     end
 
-    ngx.log(ngx.INFO, "Got ", new_image, " and err ", err )
+    if new_image == ngx.null then
+      ngx.log(ngx.INFO, "new_image boolean not found")
+    end
   until timeout >= 5 or new_image == 'true' 
 
+  -- Exit if no new image in redis for 5 seconds
   if timeout >= 5 then
     ngx.log(ngx.ERR, "Waited for new image in redis for 5 seconds, exiting now")
     break
   end
 
-  ngx.log(ngx.INFO, "Got a new image ! ")
-  -- Get image size from socket, the call to receive is blocking
-  -- TODO 4 bytes fixed size assumed integer is not a good idea
-  -- if capture server != nginx server || on other platform than armv6
+  ngx.log(ngx.INFO, "Got a new image")
+
+  -- Get image size for logging purposes
   local image_size, err = r:get('image_size')
-  ngx.log(ngx.INFO, "Received ", image_size, " long image")
+
+  if not image_size then
+    ngx.log(ngx.ERR, "Couldn't get image_size from redis, err : ",err)
+    break
+  end
+
   if image_size == 0 then
     ngx.log(ngx.ERR, "Received 0 length image, stop now")
     break
   end
 
-  -- Read image size bytes from socket
-  local image = r:get("image_data")
+  -- Get b64 encoded image data from redis
+  local image_data, err = r:get("image_data")
 
+  if not image_data then
+    ngx.log(ngx.ERR, "Couldn't get image_data from redis, err : ", err)
+    break
+  end
+ 
   -- Send base64 encoded jpeg data via the websocket to the client
-  -- bytes, err = wb:send_binary(ngx.encode_base64(image))
-  bytes, err = wb:send_binary(image)
+  local bytes, err = wb:send_binary(image_data)
   if not bytes then
     ngx.log(ngx.ERR, "Failed to send a binary frame: ", err)
-    break;
+    break
   end
+
   ngx.log(ngx.INFO, "image sent to client")
+
+  -- Inform capture that we can process a new image
   r:set('new_image', 'false')
   new_image = 'false'
 
 until done
 
+-- End communication
 local bytes, err = wb:send_close(1000, "Goodbye !")
 if not bytes then
-  ngx.log(ngx.ERR, "failed to send the close frame: ", err)
+  ngx.log(ngx.ERR, "Failed to send the closing frame : ", err)
   return
 end
 
